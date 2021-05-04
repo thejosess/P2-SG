5c27888f0171a2206668321a0d84a9ca6b6c1d03
Enrique Ruiz Limones <eruiz@granadadynamics.com>
2021-05-05 00:22:53 +0200

    el testeador

--- Link.js
+++ Link.js
@@ -1,7 +1,7 @@
-import * as THREE from '../libs/three.module.js'
-import { MTLLoader } from '../libs/MTLLoader.js'
-import { OBJLoader } from '../libs/OBJLoader.js'
-import { Vector3 } from './libs/three.module.js';
+import * as THREE from './libs/three.module.js'
+import { MTLLoader } from './libs/MTLLoader.js'
+import { OBJLoader } from './libs/OBJLoader.js'
+import { Vector3 } from './libs/three.module.js'
 
 class Link extends THREE.Object3D{
     constructor(){
--- MyScene.js
+++ MyScene.js
@@ -1,9 +1,9 @@
 
 // Clases de la biblioteca
 
-import * as THREE from '../libs/three.module.js'
-import { GUI } from '../libs/dat.gui.module.js'
-import { TrackballControls } from '../libs/TrackballControls.js'
+import * as THREE from './libs/three.module.js'
+import { GUI } from './libs/dat.gui.module.js'
+import { TrackballControls } from './libs/TrackballControls.js'
 import Stats from './libs/Stats.js';
 
 
